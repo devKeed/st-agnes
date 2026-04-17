cf318c07099f3e32f0c55c7c02313116d71ec27a
Finney Osajere <osajerefinney@gmail.com>
2026-04-17 10:41:19 +0100

    chore: remove entire repository codebase

--- apps/api/src/modules/rentals/rentals.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RentalsController } from './rentals.controller';
-import { RentalsService } from './rentals.service';
-
-@Module({
-  providers: [RentalsService],
-  controllers: [RentalsController],
-})
-export class RentalsModule {}
